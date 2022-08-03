ab4f27f1ea1aeb9e02d01cb34a068e76d9c9be87
Pawel Paczkowski <paw.pac@yahoo.com>
2022-08-03 12:19:47 -0500

    BUild winner check function

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let cellArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let randomNumRange = cellArray.length;
-
-// Select start button to switch from menu to boardgame
+let playerNums = [];
 
 const startButton = document.getElementById('start');
-
-// Select menu and boardgame (grid)
 
 const menu = document.getElementById('menu');
 const grid = document.getElementById('grid');
 
-//? Select all cells for player choice
 
 const cells = document.querySelectorAll('.cell');
 
-// Add event listener to start button to apply 'hide' to menu and remove 'hide' from boardgame
+const btnBrainX = document.getElementById('brain-x');
+const btnChipX = document.getElementById('chip-x');
+const btnBrainO = document.getElementById('brain-o');
+const btnChipO = document.getElementById('chip-o');
+
+btnBrainX.addEventListener('click', () => {
+  playerPick();
+})
+
+btnChipX.addEventListener('click', () => {
+  computerChoice();
+})
 
 startButton.addEventListener('click', () => {
   menu.classList.add('hide');
@@ -25,159 +31,90 @@ startButton.addEventListener('click', () => {
 
 //? PLAYER CHOICE
 
-//? Wrap player in one function
-
 const playerPick = () => {
-
-  //? Add event listener to all cells
 
   cells.forEach(cell => {cell.addEventListener('click', function player() {
 
     if (cell.textContent === '') {
+      cell.textContent = 'X';
+      let cellId = cell.id;
+      let cellIdNumOnly = cellId.slice(-1);
+      console.log(cellIdNumOnly);
+      playerNums.push(parseInt(cellIdNumOnly));
+      checkIfWin(playerNums, winningSequence);
+      console.log(playerNums);
+      let cellIndex = cellArray.indexOf(parseInt(cellIdNumOnly));
 
-    //? Fill cell with X
-    cell.textContent = 'X';
+      if (cellIndex > -1) {
+        cellArray.splice(cellIndex, 1);
+      }
+      
+      setTimeout(computerChoice(), 1000);
 
-    //? Cell id
-
-    let cellId = cell.id;
-    // console.log(cellId);
-
-    //? Cell ID number only
-
-    let cellIdNumOnly = cellId.slice(-1);
-    // console.log('cell id number only is ' + cellIdNumOnly);
-
-    //? create index
-
-    let cellIndex = cellArray.indexOf(parseInt(cellIdNumOnly));
-    // console.log(cellIndex);
-
-
-
-    //? Remove from array by using cellIndex only if index is found
-
-    if (cellIndex > -1) {
-      cellArray.splice(cellIndex, 1);
-
-      console.log('after player piced the array is ' + cellArray);
+      return cellArray;
     }
 
-
-
-
-
-    setTimeout(computerChoice(), 100);
-
-    
-    //? Return updated array :)
-    return cellArray;
-
-    // Check player pick NumRange and cellArray
-    // console.log('player choice is ' + cell.id.slice(-1))
-    // console.log('array: ' + cellArray);
-  }
-
   })});
+
+  checkIfWin(playerNums, winningSequence);
+
   return cellArray;
 }
-
-function printArray() {
-  console.log(cellArray);
-}
-
-
-
-
-
-
-function computerChoice() {
-
 //! COMPUTER CHOICE
+function computerChoice() {
+  let computerPick = Math.floor((Math.random() * 9) + 1);
+  let chosenCell = document.getElementById('cell' + computerPick.toString());
+  
+  if (chosenCell.textContent === '') {
+    chosenCell.textContent = 'comp'
+    let cellId = chosenCell.id;
+    let cellIdNumOnly = cellId.slice(-1);
+    let cellIndex = cellArray.indexOf(parseInt(cellIdNumOnly));
+    if (cellIndex > -1) {
+      cellArray.splice(cellIndex, 1);
+    }
 
-// Number range and cellArray needs to decrease after each move from both player and computer
+    playerPick();
 
-// let randomNumRange = 9;
-// let cellArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return cellArray;
 
-//! Generate random number between 1 and 9
-
-console.log('before computer pick the array is ' + cellArray);
-
-let computerPick = Math.floor((Math.random() * 9) + 1);
-
-//! Concatante number from computerPick to cell in order to target cell element by Id
-
-let chosenCell = document.getElementById('cell' + computerPick.toString());
-
-
-if (chosenCell.textContent === '') {
-
-
-//! Fill the cell chosen by computer
-
-chosenCell.textContent = 'comp'
-
-// TODO Remove event listenr from the cell chosen by the computer
-
-// TODO ######CODE######CODE######
-// TODO \/ \/ \/ \/ \/ \/ \/ \/ \/
-// let singleCell = document.getElementById('cell' + computerPick.toString());
-// singleCell.removeEventListener('click', playerChoice);
-// TODO /\ /\ /\ /\ /\ /\ /\ /\ /\
-
-let cellId = chosenCell.id;
-console.log('comp cell id is ' + cellId)
-
-let cellIdNumOnly = cellId.slice(-1);
-console.log('cell id from comp pick is ' + cellIdNumOnly);
-
-let cellIndex = cellArray.indexOf(parseInt(cellIdNumOnly));
-
-  //? Remove from array by using cellIndex only if index is found
-
-  if (cellIndex > -1) {
-    cellArray.splice(cellIndex, 1);
-
-    console.log('after player piced the array is ' + cellArray);
+  } else if (cellArray.length > 0) {
+    computerChoice();
   }
-
-return cellArray;
-
-} else if (cellArray.length > 0) {
-  computerChoice();
 }
 
 
 
+//! \/\/\/\/\/\/BRAINSTORM\/\/\/\/\/\/
 
-//! Decrease number range
+// const winnerSequenceArr = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
 
-// randomNumRange -= 1;
+//! /\/\/\/\/\/\BRAINSTORM/\/\/\/\/\/\
 
-//! Remove chosen cell from cellArray
 
-//TODO cellArray.splice(computerPick - 1, 1); wrong 
 
+let winningSequence = [[1, 2, 3], [4, 5, 6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+let result = true;
+
+
+function checkIfWin(contender, sequence) {
+  for (let miniArr of sequence) {
+    if (result = miniArr.every(value => {
+      return contender.includes(value)
+    })) {
+      winningStripe(miniArr);
+    }
+  }
 }
 
-
-
-
-// Check computerPick randomNumRange and cellArray
-
-// console.log('computerPick is ' + computerPick);
-// console.log('number range is ' + numberRange());
-
-
-
-// FUNCTIONS
-
-
-// Length of array
-function numberRange() {
-  return cellArray.length;
+function winningStripe(stripe) {
+  console.log(stripe);
+  for (num of stripe) {
+    let box = document.getElementById('cell' + num.toString());
+    box.classList.add('red');
+  }
 }
+
 
 
   
